c6cb2c6d1d8109207b77d1dcff78e9ff6deb97d8
MountainGod2 <admin@reid.ca>
2024-02-22 15:22:51 -0700

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ setup(
     version="0.0.1",
     packages=find_packages(),
     install_requires=["aiohttp", "aiolimiter", "python-dotenv"],
-    entry_points={"console_scripts": ["chaturbate-poller = chaturbate_api.main:main"]},
+    entry_points={"console_scripts": ["chaturbate-poller = src.chaturbate_api.main:main"]},
     author="MountainGod2",
     author_email="admin@reid.ca",
     description="Chaturbate API integration",
