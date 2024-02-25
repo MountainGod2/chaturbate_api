1e0c9b76c6a95e7bd2a846a749347cf6662ee85b
MountainGod2 <admin@reid.ca>
2024-02-24 19:31:57 -0700

    Remove unused files and update imports

--- chaturbate_api/__main__.py
+++ chaturbate_api/__main__.py
@@ -3,8 +3,8 @@ import os
 
 from dotenv import load_dotenv
 
-from chaturbate_api.api_poller import ChaturbateAPIPoller
-from chaturbate_api.exceptions import BaseURLNotFound
+from chaturbate_api.src.api_poller import ChaturbateAPIPoller
+from chaturbate_api.src.exceptions import BaseURLNotFound
 
 
 async def main():
@@ -12,9 +12,11 @@ async def main():
     load_dotenv()
     base_url = os.getenv("EVENTS_API_URL")
 
-    # Check if the base URL is set
-    if not base_url:
-        raise BaseURLNotFound
+    # Check if the base URL is set and raise exception if not
+    if base_url is None:
+        raise BaseURLNotFound(
+            "Base URL not found. Set the EVENTS_API_URL environment variable and try again."
+        )
 
     # Start the poller
     poller = ChaturbateAPIPoller(base_url)
@@ -25,6 +27,4 @@ if __name__ == "__main__":
     try:
         asyncio.run(main())
     except KeyboardInterrupt:
-        pass
-    except BaseURLNotFound:
         pass
--- chaturbate_api/src/exceptions.py
+++ chaturbate_api/src/exceptions.py
@@ -4,7 +4,4 @@
 class BaseURLNotFound(Exception):
     """Raised when the base URL of the events API is not found in the environment variables."""
 
-    print(
-        "Base URL not found. Set the EVENTS_API_URL environment variable and try again."
-    )
     pass
